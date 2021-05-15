ed5510bb1a2fa015141016e4fd175cf5f31ea2f5
Jon-Jacques Lewis <13749976+JJWLewis@users.noreply.github.com>
2021-05-15 14:14:50 +0100

    Chapter 9. Resolves #11

--- 09_abuse/abuse.py
+++ 09_abuse/abuse.py
@@ -6,6 +6,7 @@ Purpose: Rock the Casbah
 """
 
 import argparse
+import random
 
 
 # --------------------------------------------------
@@ -21,24 +22,32 @@ def get_args():
                         help='The number of insults',
                         metavar='int',
                         type=int,
-                        default=0)
+                        default=3)
 
     parser.add_argument('-a',
                         '--adjectives',
                         help='The number of adjectives',
                         metavar='int',
                         type=int,
-                        default=0)
+                        default=2)
 
     parser.add_argument('-s',
                         '--seed',
                         help='The random seed value',
-                        metavar='int',
+                        metavar='seed',
                         type=int,
-                        default=1)
+                        default=None)
 
-    return parser.parse_args()
+    args = parser.parse_args()
 
+    # Throw if inputs aren't valid
+    if args.adjectives < 1:
+        parser.error(f'--adjectives "{args.adjectives}" must be > 0')
+    
+    if args.number < 1:
+        parser.error(f'--number "{args.number}" must be > 0')
+
+    return args
 
 # --------------------------------------------------
 def main():
@@ -46,8 +55,30 @@ def main():
 
     args = get_args()
 
+    adjectives: list = """
+        bankrupt base caterwauling corrupt cullionly detestable dishonest false
+        filthsome filthy foolish foul gross heedless indistinguishable infected
+        insatiate irksome lascivious lecherous loathsome lubbery old peevish
+        rascaly rotten ruinous scurilous scurvy slanderous sodden-witted
+        thin-faced toad-spotted unmannered vile wall-eyed
+        """.strip().split()
     
+    nouns: list = """
+        Judas Satan ape ass barbermonger beggar block boy braggart butt
+        carbuncle coward coxcomb cur dandy degenerate fiend fishmonger fool
+        gull harpy jack jolthead knave liar lunatic maw milksop minion
+        ratcatcher recreant rogue scold slave swine traitor varlet villain worm
+        """.strip().split()
 
+    # Set seed and build outputs
+    random.seed(args.seed)
+
+    for _ in range(args.number):
+        noun: str = random.choice(nouns)
+        adjs: str = ', '.join(random.sample(adjectives, k=args.adjectives))
+
+        print(f'You {adjs} {noun}!')
+ 
 
 # --------------------------------------------------
 if __name__ == '__main__':
